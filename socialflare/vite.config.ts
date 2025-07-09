5aac5484c259ded149cdabad11cc3f872672c24a
Sidsmartz <siddhartht4206@gmail.com>
2025-07-09 19:48:50 +0530

    Build Error Fix #3

--- socialflare/vite.config.ts
+++ socialflare/vite.config.ts
@@ -5,14 +5,4 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   assetsInclude: ['**/*.glb'],
-  build: {
-    rollupOptions: {
-      input: {
-        main: 'index.html',
-      },
-      external: ['mongodb'],
-    },
-  },
-  // Exclude /api from Vite's build
-  publicDir: 'public',
 });
